531bea32cd153f0fa990d1d2484acc1288166816
Jorge <jorgeluisrojasb@gmail.com>
2021-01-06 12:53:01 -0400

    clientId column on sales table now correctly references to client table

--- migrations/20201216022712-add-clientId-field-to-sales-table.js
+++ migrations/20201216022712-add-clientId-field-to-sales-table.js
@@ -1,16 +1,20 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('sales', 'clientId', {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      after: 'id',
-    });
-  },
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.addColumn('sales', 'clientId', {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            defaultValue: 1,
+            after: 'id',
+            references: {
+                model: 'clients',
+                key: 'id',
+            },
+        });
+    },
 
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('sales', 'clientId');
-  },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.removeColumn('sales', 'clientId');
+    },
 };
