d84b0fe6380392c661f3a81be912f578a77139b2
Jorge <jorgeluisrojasb@gmail.com>
2021-01-14 14:21:42 -0400

    changing supplyings quantity datatype to float

---
+++ migrations/20210114181124-change-supplyings-product-quantity-to-float.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.changeColumn('supplyings', 'quantity', {
+            type: Sequelize.FLOAT,
+        });
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.changeColumn('supplyings', 'quantity', {
+            type: Sequelize.INTEGER,
+        });
+    },
+};
