ae9b861244bc9e3d9fd3f85f247d76791f9e262f
Jorge <jorgeluisrojasb@gmail.com>
2021-01-14 14:50:20 -0400

    updating quantities to round for 2 decimals

--- middlewares/checkInventory.js
+++ middlewares/checkInventory.js
@@ -1,57 +1,55 @@
-const Sales = require("../models").Sales;
-const Supplying = require("../models").Supplying;
-const Product = require("../models").Product;
-const sequelize = require("sequelize")
+const Sales = require('../models').Sales;
+const Supplying = require('../models').Supplying;
+const Product = require('../models').Product;
+const sequelize = require('sequelize');
 const Op = sequelize.Op;
 module.exports = async function (req, res, next) {
     if (req.user.permissions >= process.env.EMPLOYEE_PERMISSION) {
         let { id, name } = req.query;
 
         let queryObject = {};
-        queryObject.include = [
-            "discount"
-        ]
-        if (name) queryObject.where = {
-            name: { [Op.like]: `%${name}%` }
-        };
+        queryObject.include = ['discount'];
+        if (name)
+            queryObject.where = {
+                name: { [Op.like]: `%${name}%` },
+            };
         if (id) queryObject.where = { id };
 
         if (req.params.productId) {
             queryObject.where = {
-                id: req.params.productId
-            }
+                id: req.params.productId,
+            };
         }
-        queryObject.attributes = {
+        (queryObject.attributes = {
             include: [
                 [
                     sequelize.literal(`(
-                        SELECT IFNULL(SUM(supplying.quantity), 0) FROM supplyings as supplying
+                        SELECT IFNULL(SUM(ROUND(supplying.quantity, 2)), 0) FROM supplyings as supplying
                         WHERE supplying.productId = Product.id
                     )`),
-                    "supplyingsTotal"
+                    'supplyingsTotal',
                 ],
                 [
                     sequelize.literal(`(
-                            SELECT IFNULL(SUM(sales.quantity), 0) FROM saleproducts as sales
+                            SELECT IFNULL(SUM(ROUND(sales.quantity, 2)), 0) FROM saleproducts as sales
                             WHERE sales.productId = Product.id
                         )`),
-                    "salesTotal"
+                    'salesTotal',
                 ],
-                [sequelize.literal(`(
-                        SELECT IFNULL(SUM(supplying.quantity), 0) FROM supplyings as supplying
-                        WHERE supplying.productId = Product.id) - (SELECT IFNULL(SUM(sales.quantity), 0) FROM saleproducts as sales
+                [
+                    sequelize.literal(`(
+                        SELECT IFNULL(SUM(ROUND(supplying.quantity, 2)), 0) FROM supplyings as supplying
+                        WHERE supplying.productId = Product.id) - (SELECT IFNULL(SUM(ROUND(sales.quantity, 2)), 0) FROM saleproducts as sales
                         WHERE sales.productId = Product.id)`),
-                    "stock"
-                ]
+                    'stock',
+                ],
             ],
-            exclude: ["createdAt", "updatedAt"]
-        },
-            res.queryObject = queryObject;
+            exclude: ['createdAt', 'updatedAt'],
+        }),
+            (res.queryObject = queryObject);
         next();
+    } else {
+        res.status(401).send({ err: 'Insufficient permissions' });
     }
-    else {
-        res.status(401).send({ err: "Insufficient permissions" });
-    }
+};
 
-
-}
