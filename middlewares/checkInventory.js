f26a745912ff253c5bb0d2ccc37d6cb40203c8f6
Jorge <jorgeluisrojasb@gmail.com>
2021-04-04 00:38:27 -0400

    product stock request was changed to checkInventory middleware again because the new stock system was malfunctioning

--- middlewares/checkInventory.js
+++ middlewares/checkInventory.js
@@ -1,25 +1,25 @@
 const Sales = require("../models").Sales;
 const Supplying = require("../models").Supplying;
 const Product = require("../models").Product;
-const sequelize = require("sequelize")
+const sequelize = require("sequelize");
 const Op = sequelize.Op;
 module.exports = async function (req, res, next) {
     if (req.user.permissions >= process.env.EMPLOYEE_PERMISSION) {
-        let {id, name} = req.query;
+        let { id, name } = req.query;
 
         let queryObject = {};
         queryObject.include = [
             "discount"
-        ]
+        ];
         if (name) queryObject.where = {
-            name: {[Op.like]: `%${name}%`}
+            name: { [Op.like]: `%${name}%` }
         };
-        if (id) queryObject.where = {id};
+        if (id) queryObject.where = { id };
 
         if (req.params.productId) {
             queryObject.where = {
                 id: req.params.productId
-            }
+            };
         }
         queryObject.attributes = {
             include: [
@@ -41,7 +41,7 @@ module.exports = async function (req, res, next) {
                         SELECT IFNULL(SUM(ROUND(supplying.quantity, 3)), 0) FROM supplyings as supplying
                         WHERE supplying.productId = Product.id) - (SELECT IFNULL(SUM(ROUND(sales.quantity,3 )), 0) FROM saleproducts as sales
                         WHERE sales.productId = Product.id)`),
-                    "stock"
+                    "inventario"
                 ]
             ],
             exclude: ["createdAt", "updatedAt"]
@@ -50,8 +50,8 @@ module.exports = async function (req, res, next) {
         next();
     }
     else {
-        res.status(401).send({err: "Insufficient permissions"});
+        res.status(401).send({ err: "Insufficient permissions" });
     }
 
 
-}
+};
